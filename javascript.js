21262b6a0291aa175785e3b7b6843988e6fbea26
Cesar <cesr.sntn@gmail.com>
2022-07-06 10:56:43 -0400

    Create calculator directory with files and complete basic calc layout & css
    Created index, css, and js files with html boiler. Created html to hold almost everything that should be in js. Used css to create the calculator layout and added effects. Created basic functions to handle operations in js. Need to add js code to allow functionality between html and js (addEventListeners).

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const sum = ((a,b) => a+b);
+
+const sub = ((a,b) => a-b);
+
+const mult = ((a,b) => a*b);
+
+const divide = ((a,b) => a/b);
+
+const sqrt = ((a,b) => Math.sqrt(a));
+
+const sq = ((a,b) => Math.pow(a,2));
+
+
+const problem = document.querySelector("#cont1")
+const answer = document.querySelector("#cont2")
+
+
